41cf12d4ea6d4b0d8e34a3a9087b806283600efe
kseniakaratchenia <62858256+kseniakaratchenia@users.noreply.github.com>
2023-02-08 15:01:23 -0800

    newTask1

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a = 1;
+        int b = 4;
+
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c1 = 99;
+        char h1 = 104;
+        char a1 = 97;
+        char r1 = 114;
     }
 }
